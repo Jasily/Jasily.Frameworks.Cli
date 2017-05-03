8cc9ed532b8a7017830cb61f9de8e840e47291a1
赖巍 <skyoflw@gmail.com>
2017-05-04 02:33:47 +0800

    refactor.

--- Jasily.Frameworks.Cli.Standard/Core/Session.cs
+++ Jasily.Frameworks.Cli.Standard/Core/Session.cs
@@ -12,8 +12,7 @@ namespace Jasily.Frameworks.Cli.Core
     internal class Session : ISession
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly List<CommandRouter> _routers = new List<CommandRouter>();
-        private ICommand _command;
+        private readonly List<ResolvedNode> _nodes = new List<ResolvedNode>();
 
         public Session(IServiceProvider serviceProvider)
         {
@@ -27,15 +26,7 @@ namespace Jasily.Frameworks.Cli.Core
         public void DrawUsage()
         {
             var drawer = this._serviceProvider.GetRequiredService<IUsageDrawer>();
-
-            if (this._command != null)
-            {
-                drawer.DrawParameter(this._command.Properties, this._command.ParameterConfigurations);
-            }
-            else
-            {
-                drawer.DrawRouter(this._routers.Last().CommandsProperties);
-            }
+            this._nodes.Last().Draw(drawer);
         }
 
         public void Termination()
@@ -50,13 +41,47 @@ namespace Jasily.Frameworks.Cli.Core
 
         public void AddRouter(CommandRouter router)
         {
-            this._command = null;
-            this._routers.Add(router);
+            this._nodes.Add(new ResolvedNode(router));
         }
 
         public void AddCommand(ICommand command)
         {
-            this._command = command;
+            this._nodes.Add(new ResolvedNode(command));
+        }
+
+        private struct ResolvedNode
+        {
+            public ResolvedNode(CommandRouter router)
+            {
+                this.IsRouter = true;
+                this.Router = router;
+                this.Command = null;
+            }
+
+            public ResolvedNode(ICommand command)
+            {
+                this.IsRouter = false;
+                this.Router = default(CommandRouter);
+                this.Command = command;
+            }
+
+            public bool IsRouter { get; }
+
+            public CommandRouter Router { get; }
+
+            public ICommand Command { get; }
+
+            public void Draw(IUsageDrawer drawer)
+            {
+                if (this.IsRouter)
+                {
+                    drawer.DrawRouter(this.Router.CommandsProperties);
+                }
+                else
+                {
+                    drawer.DrawParameter(this.Command.Properties, this.Command.ParameterConfigurations);
+                }
+            }
         }
     }
 }
